77fead6b45bd927b758d35f3b6e47cc0175e96e4
benjaminpaez <benjaapaez9@gmail.com>
2023-11-26 12:21:25 -0300

    add changes

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { type Question } from "../types";
 import confetti from "canvas-confetti";
-import { persist } from "zustand/middleware";
+import { persist, devtools } from "zustand/middleware";
 
 interface State {
   questions: Question[];
@@ -13,60 +13,61 @@ interface State {
   reset: () => void
 }
 
-export const useQuestionsStore = create<State>()(persist((set, get) => {
+export const useQuestionsStore = create<State>()(devtools(persist((set, get) => {
   return {
+    loading: false,
     questions: [],
     currentQuestion: 0,
 
     fetchQuestions: async (limit: number) => {
-      const res = await fetch("http://localhost:5173/data.json");
-      const json = await res.json();
-      const questions = json.sort(() => Math.random() - 0.5).slice(0, limit);
+      const res = await fetch("http://localhost:5173/data.json")
+      const json = await res.json()
+      const questions = json.sort(() => Math.random() - 0.5).slice(0, limit)
 
-      set({ questions });
+      set({ questions }, false, 'FETCH_QUESTIONS')
     },
 
     selectAnswer: (questionId: number, answerIndex: number) => {
-      const { questions } = get();
-      const newQuestions = structuredClone(questions);
+      const { questions } = get()
+      const newQuestions = structuredClone(questions)
 
-      const questionIndex = newQuestions.findIndex((q) => q.id == questionId);
-      const questionInfo = newQuestions[questionIndex];
+      const questionIndex = newQuestions.findIndex((q) => q.id == questionId)
+      const questionInfo = newQuestions[questionIndex]
 
-      const isCorrectUserAnswer = questionInfo.correctAnswer == answerIndex;
+      const isCorrectUserAnswer = questionInfo.correctAnswer == answerIndex
 
-      if (isCorrectUserAnswer) confetti();
+      if (isCorrectUserAnswer) confetti()
       newQuestions[questionIndex] = {
         ...questionInfo,
         isCorrectUserAnswer,
         userSelectedAnswer: answerIndex,
       };
 
-      set({ questions: newQuestions });
+      set({ questions: newQuestions }, false, 'SELECT_ANSWER');
     },
 
     goNextQuestion: () => {
-      const { currentQuestion, questions } = get();
-      const nextQuestion = currentQuestion + 1;
+      const { currentQuestion, questions } = get()
+      const nextQuestion = currentQuestion + 1
 
       if (nextQuestion < questions.length) {
-        set({ currentQuestion: nextQuestion });
+        set({ currentQuestion: nextQuestion }, false, 'GO_NEXT_QUESTION')
       }
     },
 
     goPreviousQuestion: () => {
-      const { currentQuestion } = get();
-      const previousQuestion = currentQuestion - 1;
+      const { currentQuestion } = get()
+      const previousQuestion = currentQuestion - 1
 
       if (previousQuestion >= 0) {
-        set({ currentQuestion: previousQuestion });
+        set({ currentQuestion: previousQuestion }, false, 'GO_PREVIOUS_QUESTION')
       }
     },
 
     reset: () => {
-        set({ currentQuestion: 0, questions: []})
+        set({ currentQuestion: 0, questions: []}, false, 'RESET')
     }
-  };
+  }
 },{
     name: 'questions',
-}));
+})))
